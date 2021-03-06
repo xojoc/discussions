26d8849e416fb8742f31388a377d8a3aca4dc5fa
Alexandru Cojocaru <hi@xojoc.pw>
2021-03-06 23:36:56 +0100

    Test async view

--- web/views.py
+++ web/views.py
@@ -3,7 +3,7 @@ import datetime
 from django.http import HttpResponse
 
 
-def index(request):
+async def index(request):
     now = datetime.datetime.now()
     html = '<html><body>It is now %s.</body></html>' % now
     return HttpResponse(html)
