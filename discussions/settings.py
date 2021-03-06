6957d02ee3ef7656126fdeba50298b3988a94451
Alexandru Cojocaru <hi@xojoc.pw>
2021-03-06 23:06:55 +0100

    Add ALLOWED_HOST

--- discussions/settings.py
+++ discussions/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST')]
 
 # Application definition
 
