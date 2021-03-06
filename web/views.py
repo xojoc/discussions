2463b3a0df7dc11b1c6677612460e9aa48ead7f3
Alexandru Cojocaru <hi@xojoc.pw>
2021-03-06 23:36:00 +0100

    Add web app

---
+++ web/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+import datetime
+from django.http import HttpResponse
+
+
+def index(request):
+    now = datetime.datetime.now()
+    html = '<html><body>It is now %s.</body></html>' % now
+    return HttpResponse(html)
