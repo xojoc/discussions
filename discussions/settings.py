3d0c42348dc31c4052e2fe9d15f5b2e9ef51ee9c
Alexandru Cojocaru <hi@xojoc.pw>
2021-03-07 18:19:58 +0100

    Log to console

--- discussions/settings.py
+++ discussions/settings.py
@@ -154,5 +154,26 @@ CELERY_BEAT_SCHEDULE = {
 
 
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+    'loggers': {
+        'web': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+}
+
 if os.environ.get('DJANGO_DEVELOPMENT'):
     from .settings_dev import *
