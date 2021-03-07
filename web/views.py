3bc281442a3906866d88968cb6cb52995a5595c5
Alexandru Cojocaru <hi@xojoc.pw>
2021-03-07 12:53:38 +0100

    Test write to DB

--- web/views.py
+++ web/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 import datetime
 from django.http import HttpResponse
+from . import models
+from django.utils.timezone import make_aware
 
-
-async def index(request):
+def index(request):
     now = datetime.datetime.now()
     html = '<html><body>It is now %s.</body></html>' % now
+
+    models.Discussion(
+        platform_id='z',
+        comment_count=3,
+        score=4,
+        created_at=make_aware(now),
+        scheme_of_story_url='http',
+        schemeless_story_url='xojoc.pw',
+        canonical_story_url=None,
+        title='Title').save()
+
     return HttpResponse(html)
