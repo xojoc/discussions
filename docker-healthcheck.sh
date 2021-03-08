86a2b7e67c763b26b2e3a4ed3548959e9f1c6cd4
Alexandru Cojocaru <hi@xojoc.pw>
2021-03-09 00:09:10 +0100

    Test healthcheck

--- docker-healthcheck.sh
+++ docker-healthcheck.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-curl --fail http://localhost:80/ || exit 1
+#curl --fail http://localhost:80/ || exit 1
 celery -A discussions status --json | grep -q '"ok"' || exit 1
 # how to check if celery beat is running?
