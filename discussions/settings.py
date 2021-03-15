9296ec09845ab2ec7236c4915b09b01dcc6d1667
Alexandru Cojocaru <hi@xojoc.pw>
2021-03-15 22:40:20 +0000

    Slower tasks

--- discussions/settings.py
+++ discussions/settings.py
@@ -145,11 +145,11 @@ CELERY_TASK_ACKS_LATE = True
 CELERY_BEAT_SCHEDULE = {
     'fetch_all_hn_discussions': {
         'task': 'web.hn.fetch_all_hn_discussions',
-        'schedule': APP_CELERY_TASK_MAX_TIME * 1.1,
+        'schedule': APP_CELERY_TASK_MAX_TIME * 2,
     },
     'fetch_hn_updates': {
         'task': 'web.hn.fetch_updates',
-        'schedule': 30,
+        'schedule': 60,
     },
 }
 
