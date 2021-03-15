ecae72876fd6a0a5190a7ea9bf6f3bb712562996
Alexandru Cojocaru <hi@xojoc.pw>
2021-03-15 23:24:38 +0000

    Disable celery

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -16,10 +16,10 @@ echo "Apply database migrations"
 python manage.py migrate --noinput
 
 echo "Run Celery"
-celery -A discussions worker -l warning -P gevent -c 500 &
+# celery -A discussions worker -l warning -P gevent -c 500 &
 
 echo "Run Celery Beat"
-celery -A discussions  beat -l info &
+# celery -A discussions  beat -l info &
 
 echo "Starting server"
 python manage.py runserver 0.0.0.0:80 &
