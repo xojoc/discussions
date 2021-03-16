07e29b5a461cbf86b7d465afa9d3b6ced770b5ce
Alexandru Cojocaru <hi@xojoc.pw>
2021-03-16 22:53:36 +0000

    Urls

--- discussions/urls.py
+++ discussions/urls.py
@@ -5,5 +5,5 @@ from django.urls import re_path, include, path
 urlpatterns = [
     path('', include('web.urls')),
     path('admin/', admin.site.urls),
-    re_path(r'^admin/statuscheck/', include('celerybeat_status.urls', namespace='celerybeat_status'))),
+    re_path(r'^admin/statuscheck/', include('celerybeat_status.urls', namespace='celerybeat_status')),
 ]
