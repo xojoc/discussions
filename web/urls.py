76a2ac7d616661b2123d383c13ab44d0b7bc8d67
Alexandru Cojocaru <hi@xojoc.pw>
2021-08-31 21:08:08 +0200

    sentry debug

--- web/urls.py
+++ web/urls.py
@@ -3,6 +3,12 @@ from . import views
 
 app_name = 'web'
 
+
+def sentry_trigger_error(request):
+    _ = 1 / 0
+
+
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('sentry-debug/', sentry_trigger_error)
 ]
