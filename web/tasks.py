998568c3a42874c4fd99a44bc8ba880d0a66860c
Alexandru Cojocaru <hi@xojoc.pw>
2021-09-06 21:54:35 +0200

    delete useless discussions

--- web/tasks.py
+++ web/tasks.py
@@ -1,6 +1,6 @@
 # from . import http
 # from celery import shared_task
 import logging
-from web import hn, lobsters, reddit, statistics # noqa F401
+from web import hn, lobsters, reddit, statistics, discussions # noqa F401
 
 logger = logging.getLogger(__name__)
