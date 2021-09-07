ee1d784c63a6aeb075818ad3fde13013fb133886
Alexandru Cojocaru <hi@xojoc.pw>
2021-09-07 21:03:18 +0200

    redeploy

--- discussions/settings.py
+++ discussions/settings.py
@@ -152,6 +152,7 @@ CELERY_TASK_ACKS_LATE = True
 
 
 # xojoc: find a way to create default schedules for freshly installed apps
+
 CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
 
 
