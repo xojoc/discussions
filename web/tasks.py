658efd860e8e0bbcc839775c5069dfff83e660e9
Alexandru Cojocaru <hi@xojoc.pw>
2021-10-17 23:53:07 +0200

    ltu celery

--- web/tasks.py
+++ web/tasks.py
@@ -2,5 +2,6 @@
 # from celery import shared_task
 import logging
 from web import hn, lobsters, reddit, statistics, discussions, twitter  # noqa F401
+from web import ltu  # noqa F401
 
 logger = logging.getLogger(__name__)
