61d51ae5df6b2c8f110e98a5b92d18746cffe97a
Alexandru Cojocaru <hi@xojoc.pw>
2021-10-28 01:15:49 +0200

    encode

--- web/templates/web/discussions_bookmarklet.js
+++ web/templates/web/discussions_bookmarklet.js
@@ -1,1 +1,1 @@
-javascript: (function () { var url = 'https://discu.eu/q/' + encodeURI(window.location.href); var win = window.open(url, '_blank'); win.focus(); })();
+javascript: (function () { var url = 'https://discu.eu/q/' + window.location.href; var win = window.open(url, '_blank'); win.focus(); })();
