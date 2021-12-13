bf14c0e1da43b839d349d27bb614ea57fcc4ec3d
Alexandru Cojocaru <hi@xojoc.pw>
2021-12-13 16:14:49 +0100

    change sitemap extraction

--- web/sitemaps.py
+++ web/sitemaps.py
@@ -1,6 +1,6 @@
 from django.contrib.sitemaps import Sitemap
 from .models import Discussion
-from django.db.models import Sum, Count, Max
+# from django.db.models import Sum, Count, Max
 from . import util
 
 
@@ -10,20 +10,26 @@ class DiscussionsSitemap(Sitemap):
 
     def items(self):
         q = Discussion.objects.\
-            exclude(canonical_story_url__isnull=True).\
-            values('canonical_story_url').\
-            annotate(comment_count=Sum('comment_count'),
-                     discussion_count=Count('platform_id'),
-                     entry_updated_at=Max('entry_updated_at'),
-                     scheme_of_story_url=Max('scheme_of_story_url')).\
             filter(comment_count__gte=5).\
             order_by('canonical_story_url')
-        # qstr = str(q.query)
-        return q[:3_000]
+
+        # xojoc: Group By is too slow. Disable it for now.
+        #        fix extraction when the single page model will be introduced
+
+        # exclude(canonical_story_url__isnull=True).\
+        # values('canonical_story_url').\
+        # annotate(comment_count=Sum('comment_count'),
+        #          discussion_count=Count('platform_id'),
+        #          entry_updated_at=Max('entry_updated_at'),
+        #          scheme_of_story_url=Max('scheme_of_story_url')).\
+        # filter(comment_count__gte=5).\
+        # order_by('canonical_story_url')
+
+        return q
 
     def lastmod(self, obj):
-        return obj['entry_updated_at']
+        return obj.entry_updated_at
 
     def location(self, obj):
-        url = obj['scheme_of_story_url'] + '://' + obj['canonical_story_url']
+        url = obj.scheme_of_story_url + '://' + obj.canonical_story_url
         return util.discussions_canonical_url(url, with_domain=False)
