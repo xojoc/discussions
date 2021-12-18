f92eea92bb28b073b77fc9df7369c4c3353ddd9b
Alexandru Cojocaru <hi@xojoc.pw>
2021-12-18 09:39:13 +0100

    disable celery healthcheck for now

--- docker-healthcheck.sh
+++ docker-healthcheck.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #curl --fail http://localhost:80/ > /dev/null || exit 1
 wget -q --spider http://localhost:80/ || exit 1
-celery -A discussions status --json -t 30 | grep -q '"ok"' || exit 2
+# celery -A discussions status --json -t 30 | grep -q '"ok"' || exit 2
 # todo: how to check if celery beat is running?
