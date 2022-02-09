20cb8be94856905937cd3b68c42dee5affa9d399
Alexandru Cojocaru <hi@xojoc.pw>
2022-02-09 18:08:52 +0100

    limit index now

--- web/indexnow.py
+++ web/indexnow.py
@@ -17,6 +17,9 @@ def process_discussion(sender, instance, created, **kwargs):
     if not created:
         return
 
+    if instance.comment_count < 100:
+        return
+
     indexnow.delay(util.discussions_canonical_url(instance.story_url))
 
 
@@ -29,6 +32,8 @@ def indexnow(url):
         print(f"IndexNow: {url}")
         return
 
-    r = requests.get("https://bing.com/indexnow", params={"url": url, "key": key})
+    r = requests.get(
+        "https://bing.com/indexnow", params={"url": url, "key": key}
+    )
     if not r.ok:
         logger.error(f"IndeNow failed: {url} {r.status_code} {r.reason}")
