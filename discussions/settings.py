9a4850037b64b00e62bfee4c155b0e776f8a3541
Alexandru Cojocaru <hi@xojoc.pw>
2022-10-07 15:35:45 +0200

    static files

--- discussions/settings.py
+++ discussions/settings.py
@@ -179,7 +179,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 STATIC_URL = "/static/"
 STATIC_ROOT = STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+
 
 USERAGENT = "Discu.eu bot/0.1"
 
