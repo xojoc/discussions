b4e3fd43e4e0ffb5ab3472e86b564956f9f32e6f
Alexandru Cojocaru <hi@xojoc.pw>
2022-11-09 19:02:47 +0100

    s

--- web/db.py
+++ web/db.py
@@ -181,7 +181,6 @@ def admin_send_recap_email(self):
 
     twitter_followers_count = twitter.get_followers_count(twitter_usernames)
     mastodon_followers_count = mastodon.get_followers_count(mastodon_usernames)
-
     users_count = models.CustomUser.objects.count()
 
     users_premium_count = (
